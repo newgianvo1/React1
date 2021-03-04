d76019da7cb97dd6aa3b61078056588f36d564f9
gian159753 <yandel162011@gmail.com>
2021-03-04 13:13:14 -0500

    footerv3 arreglado 4Marzo

--- src/Components/Body/BodyPage.js
+++ src/Components/Body/BodyPage.js
@@ -6,47 +6,51 @@ import './Header.css'
 const Body = () => {
 
 
-    return ( 
-        
-     
+    return (
 
 
-       <main>
+
+
+        <main>
+            
 
             <div className="topnav">
 
-            <br></br>
-            <img className="ImageNSV" alt="NSV" width={"120px"} src={window.location.origin + '/img/logo.png'}  />
+                <br></br>
+                <img className="ImageNSV" alt="NSV" width={"120px"} src={window.location.origin + '/img/logo.png'} />
 
-            <a className="active" href="#home">Home</a>
-            <a href="#research">Research Institute Projects</a>
-            <a href="#press">Press</a>
-            <a href="#volunteer">Volunteer</a>
-            <a href="#contact">Contact</a>
-            <a className="elipse" href="#donate">Donate</a>
-            <br></br>
-            <br></br>
-          
+                <a className="active" href="#home">Home</a>
+                <a href="#research">Research Institute Projects</a>
+                <a href="#press">Press</a>
+                <a href="#volunteer">Volunteer</a>
+                <a href="#contact">Contact</a>
+                <a className="elipse" href="#donate">Donate</a>
+                <br></br>
+                <br></br>
+
 
             </div>
-          
-        
-        <section className="container-fluid" >
-            
-        <img className="ImageNSV1" alt="NSV" width={"160px"} src={window.location.origin + '/img/icon.png'}  />
 
-        </section>
 
-        <section className="main_container">
-            <div>
-                <h1>Research Institute Projects</h1>
-            </div>
+            <section className="container-fluid" >
+                <div className="rowImg">
 
-          {/*************************/
+                    <img className="ImageNSV1" alt="NSV" width={"160px"} src={window.location.origin + '/img/icon.png'} />
+
+                </div>
+
+            </section>
+
+            <section className="main_container">
+                <div>
+                    <h1>Research Institute Projects</h1>
+                </div>
+
+                {/*************************/
            /*       Lantern         */
            /*************************/}
 
-              <div className="odd" >
+                <div className="odd" >
                     <ul className="first">
                         <li><h5>Lantern</h5></li>
                         <li><p>Designed for parents,teachers and school counselors</p></li>
@@ -59,33 +63,33 @@ const Body = () => {
                             to know which type of violence those behaviors might trigger?</p>
                         </li>
                     </ul>
-              </div>
+                </div>
 
-              {/*************************/
+                {/*************************/
                /*        Athena         */
                /*************************/}
 
-              <div className="even" >
-                  <ul className="first">
-                      <li><h5>Athena</h5></li>
-                      <li><p>Designed for teachers, school counselors, administrators, law enforcement
+                <div className="even" >
+                    <ul className="first">
+                        <li><h5>Athena</h5></li>
+                        <li><p>Designed for teachers, school counselors, administrators, law enforcement
                           and researchers/policy analyst.</p></li>
-                      <li><button type="button" className="btn btn-secondary">Open Tool</button></li>
-                  </ul>
+                        <li><button type="button" className="btn btn-secondary">Open Tool</button></li>
+                    </ul>
 
-                  <ul className="second">
-                      <li>
-                          <p>Discover school violence patterns. Explore how 451 school shooting communities,shooting incidents,police reports,shooter behaviors,shooter
+                    <ul className="second">
+                        <li>
+                            <p>Discover school violence patterns. Explore how 451 school shooting communities,shooting incidents,police reports,shooter behaviors,shooter
                             medical histories, and school structures, provide reduction insight and strategies</p>
-                      </li>
-                  </ul>
-              </div>
+                        </li>
+                    </ul>
+                </div>
 
-              {/*************************/
+                {/*************************/
                /*  Covid School Rentry  */
                /*************************/}
 
-              <div className="odd" >
+                <div className="odd" >
                     <ul className="first">
                         <li><h5>COVID School Rentry</h5></li>
                         <li><p>Designed to prepare schools for post-COVID schoool violence</p></li>
@@ -98,106 +102,112 @@ const Body = () => {
                         Parents and schools knowing what to expect and having good interventions
                         ready can reduce school violence</p></li>
                     </ul>
-              </div>
+                </div>
 
-              {/*************************/
+                {/*************************/
                /*  Language of violence */
                /*************************/}
 
-              <div className="even" >
-                  <ul className="first">
-                      <li><h5 className="TagLanguage">Language of Violence</h5></li>
-                      <li><p>Designed to provide insights into what correlations between schools shooters
+                <div className="even" >
+                    <ul className="first">
+                        <li><h5 className="TagLanguage">Language of Violence</h5></li>
+                        <li><p>Designed to provide insights into what correlations between schools shooters
                       and violent domestic organization mean.</p></li>
-                      <li><button type="button" className="btn btn-secondary">Open Tool</button></li>
-                  </ul>
+                        <li><button type="button" className="btn btn-secondary">Open Tool</button></li>
+                    </ul>
 
-                  <ul className="second">
-                      <li><p>Explore language patterns of 451 school shooters,including what they say,
-                      how they say it , and the group language of those shooters identify with including
+                    <ul className="second">
+                        <li><p>Explore language patterns of 451 school shooters,including what they say,
+                        how they say it , and the group language of those shooters identify with including
                       Domestic terrorists, white nationalists, and 940 hate groups (SPLC).</p></li>
-                  </ul>
-              </div>
+                    </ul>
+                </div>
 
-        </section>
+            </section>
 
 
 
-        <div className="container-fluid-footer" >
+            <div className="container-fluid-footer" >
 
-      
-            <div className="row text-white font-weight-bold text-center pt-3 pb-4">
-                <div className="col-12  col-lg-3 col-md-3  border-right footer">
-                <a  className="text-white" href="#"><p>Home</p></a>
+
+                <div className="row text-white font-weight-bold text-center pt-3 pb-4">
+                    <div className="col-12  col-lg-3 col-md-3  border-right footer">
+
                         <p>NoSchoolViolence</p>
                         <p>Seattle | Washington</p>
 
+                    </div>
+                    <div className="col-12 col-lg-3 col-md-3    border-right footer">
+                        <a className="text-white" href="#"><p>Research Institute of project</p></a>
+
+                    </div>
+                    <div className="col-12  col-lg-3 col-md-3   border-right footer">
+                        <a className="text-white" href="#"><p className="text-green">Press</p></a>
+                    </div>
+                    <div className=" col-12 col-xs-12  col-lg-3 col-md-3   footer">
+
+                        <a className="text-white" ><p>Action</p></a>
+                        <a className="text-white" href="#"><p>Volunteer</p></a>
+                        <a className="text-white" href="#"><p>Contact</p></a>
+                        <a className="text-white" href="#"><p>Donate</p></a>
+
+                    </div>
+
+
                 </div>
-                <div className="col-12 col-lg-3 col-md-3    border-right footer">
-                <a  className="text-white" href="#"><p>Research Institute of project</p></a>
-                
-                </div>
-                <div className="col-12  col-lg-3 col-md-3   border-right footer">
-                <a className="text-white" href="#"><p className="text-green">Press</p></a>
-                </div>
-                <div className=" col-12 col-xs-12  col-lg-3 col-md-3   footer">
-                
-                <a className="text-white" ><p>Action</p></a>
-                <a  className="text-white" href="#"><p>Volunter</p></a>
-                <a  className="text-white" href="#"><p>Contact</p></a>
-                <a  className="text-white" href="#"><p>Donate</p></a>
 
-                </div>
-
-  
-  </div>
-  <div className="col-12 text-center text-white font-weight-bold pb-4">
-
-          
+                <div className="col-12 text-center text-white font-weight-bold pb-4">
 
 
-                <div className="col-md-12">
+
+                    <div className="col-md-12">
 
 
                         <div className="pull-right flip social-media " role="navigation">
-                                <ul className="nav-list-inline">
-                                    <li className="list-inline-item facebook">
-                                        <a href="https://www.facebook.com/noschoolviolence.org/">
-                                            <i className="text-white fab fa-facebook-f"></i>
+                            <ul className="nav-list-inline">
+                                <li className="list-inline-item facebook">
+                                    <a href="https://www.facebook.com/noschoolviolence.org/">
+                                        <i className="text-white fab fa-facebook-f"></i>
 
-                                        </a>
-                                    </li>
-                                    
-                                    <li className="list-inline-item instagram">
-                                        <a href="https://www.instagram.com/noschoolviolence/">
+                                    </a>
+                                </li>
+
+                                <li className="list-inline-item instagram">
+                                    <a href="https://www.instagram.com/noschoolviolence/">
                                         <i className="text-white fab fa-instagram"></i>
-                                        </a>
-                                    </li>
-                                    <li className="list-inline-item twiter">
-                                        <a href="https://twitter.com/NoScholViolence">
+                                    </a>
+                                </li>
+                                <li className="list-inline-item twiter">
+                                    <a href="https://twitter.com/NoScholViolence">
                                         <i className="text-white fab fa-twitter"></i>
-                                        </a>
-                                    </li>
-                                    
-                                    <li className="list-inline-item linkedin">
-                                        <a href="https://www.linkedin.com/company/noschoolviolence-org">
-                                        <i className=" text-white fab fa-linkedin-in"></i>   
-                                        </a>
-                                        </li>	
-                                </ul> 
+                                    </a>
+                                </li>
+
+                                <li className="list-inline-item linkedin">
+                                    <a href="https://www.linkedin.com/company/noschoolviolence-org">
+                                        <i className=" text-white fab fa-linkedin-in"></i>
+                                    </a>
+                                </li>
+                            </ul>
                         </div>
 
+                    </div>
+
+                    <div className="col-12 text-center text-white">
+                        <p>contact@NoSchoolViolence.org</p>
+                    </div>
+
+                    <span>Copyright 2021 - All rights reserved</span>
+
+                </div>
+
             </div>
-    <span>Copyright 2021 - All rights reserved</span>
-  
-      </div>
-</div>
 
 
-      </main>
-     );
-   
-     
+        </main>
+    );
+
+
 }
 
 export default Body;
