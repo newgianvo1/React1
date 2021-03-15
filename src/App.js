76dcd02b5e8da56e4d98e8205bd404f8ce668d85
MelanyNadine <55468076+MelanyNadine@users.noreply.github.com>
2021-03-14 21:58:22 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Body from './Components/Body/BodyPage'
+import Footer from './Components/Footer/Footer'
+import Header from './Components/Header/Header'
+
 function App() {
   return (
    <div className="App">
+    <Header/>
     <Body/>
+    <Footer/>
    </div>
   );
 }
